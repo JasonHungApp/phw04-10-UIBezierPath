939d7e48f318fb7e55f248f2838ff402142469f0
jasonhung <jasonhungckm5801@gmail.com>
2024-03-24 06:17:39 +0800

    add magicCircleLayers

---
+++ phw04-10-UIBezierPath/GoBoardView.swift
@@ -0,0 +1,20 @@
+//
+//  GoBoardView.swift
+//  phw04-10-UIBezierPath
+//
+//  Created by jasonhung on 2024/3/21.
+//
+
+import UIKit
+
+class GoBoardView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ phw04-10-UIBezierPath/MagicCirclePart.swift
@@ -0,0 +1,8 @@
+//
+//  MagicCirclePart.swift
+//  phw04-10-UIBezierPath
+//
+//  Created by jasonhung on 2024/3/24.
+//
+
+import Foundation
